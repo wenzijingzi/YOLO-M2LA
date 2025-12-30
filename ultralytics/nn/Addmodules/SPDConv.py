3f3ad2044ebb9ad520d89bf97a21f4b4de749c2f
wenzijingzi <96813968+wenzijingzi@users.noreply.github.com>
2025-12-30 15:47:50 +0800

    Add files via upload

---
+++ ultralytics/nn/Addmodules/SPDConv.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+
+__all__ = ['SPDConv']
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+
+class SPDConv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=3, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        c1 = c1 * 4
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        x = torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        x = torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
+        return self.act(self.conv(x))
+
+if __name__ == "__main__":
+    # Generating Sample image
+    image_size = (1, 64, 240, 240)
+    image = torch.rand(*image_size)
+
+    # Model
+    Model = SPDConv(64, 64, 3, s=1)
+
+    out = Model(image)
+    print(out.size())
